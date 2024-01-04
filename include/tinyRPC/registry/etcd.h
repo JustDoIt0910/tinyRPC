07c57eaeec1426408f10b99d897eefe1a6db0894
JustDoIt0910 <1396812324@qq.com>
2024-01-04 14:54:24 +0800

    feat: add nlohmann/json

---
+++ include/tinyRPC/registry/etcd.h
@@ -0,0 +1,8 @@
+//
+// Created by just do it on 2024/1/4.
+//
+
+#ifndef TINYRPC_ETCD_H
+#define TINYRPC_ETCD_H
+
+#endif //TINYRPC_ETCD_H
---
+++ include/tinyRPC/registry/registry.h
@@ -0,0 +1,8 @@
+//
+// Created by just do it on 2024/1/4.
+//
+
+#ifndef TINYRPC_REGISTRY_H
+#define TINYRPC_REGISTRY_H
+
+#endif //TINYRPC_REGISTRY_H
